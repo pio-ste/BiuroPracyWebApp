26cd9b3eada25454e2786e033bf309266f4dce24
pioste <pioste97@gmail.com>
2019-12-14 03:14:16 +0100

    commit 7

--- src/main/java/com/biuropracy/demo/service/CourseService.java
+++ src/main/java/com/biuropracy/demo/service/CourseService.java
@@ -1,12 +1,59 @@
 package com.biuropracy.demo.service;
 
+import com.biuropracy.demo.model.Course;
+import com.biuropracy.demo.model.User;
 import com.biuropracy.demo.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourseService {
 
     @Autowired
     CourseRepository courseRepository;
+
+    public Course getCourseById(Integer id) {
+        Optional<Course> courseOpt = courseRepository.findById(id);
+        if (courseOpt.isPresent()) {
+            return courseOpt.get();
+        } else {
+            throw new RuntimeException("ID kursu nie znalezione.");
+        }
+    }
+
+    public Course updateCourse(Course course) {
+        Optional<Course> courseOpt = courseRepository.findById(course.getIdCourse());
+        if (courseOpt.isPresent()) {
+            Course newCourse = courseOpt.get();
+            newCourse.setCourseName(course.getCourseName());
+            newCourse.setOrganizer(course.getOrganizer());
+            newCourse.setCourseMonth(course.getCourseMonth());
+            newCourse.setCourseYear(course.getCourseYear());
+
+            newCourse = courseRepository.save(newCourse);
+
+            return newCourse;
+        } else {
+            course = courseRepository.save(course);
+            return course;
+        }
+    }
+
+    public Course createCourse(Course course, User user) {
+        course.setUser(user);
+        course = courseRepository.save(course);
+        return course;
+    }
+
+    public void deleteCourseById(Integer id) {
+        Optional<Course> courseOpt = courseRepository.findById(id);
+        if (courseOpt.isPresent()) {
+            courseRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("ID kursu nie znalezione.");
+        }
+    }
+
 }
--- src/main/java/com/biuropracy/demo/service/EducationService.java
+++ src/main/java/com/biuropracy/demo/service/EducationService.java
@@ -1,12 +1,62 @@
 package com.biuropracy.demo.service;
 
+import com.biuropracy.demo.model.Education;
+import com.biuropracy.demo.model.User;
 import com.biuropracy.demo.repository.EducationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class EducationService {
 
     @Autowired
     EducationRepository educationRepository;
+
+    public Education getEducationById(Integer id) {
+        Optional<Education> educationOpt = educationRepository.findById(id);
+        if (educationOpt.isPresent()) {
+            return educationOpt.get();
+        } else {
+            throw new RuntimeException("Id education nie znalezione");
+        }
+    }
+
+    public Education updateEducation(Education education) {
+        Optional<Education> educationOpt = educationRepository.findById(education.getIdEducation());
+        if (educationOpt.isPresent()) {
+            Education newEducation = educationOpt.get();
+            newEducation.setSchoolName(education.getSchoolName());
+            newEducation.setEducationLevel(education.getEducationLevel());
+            newEducation.setSpecialization(education.getSpecialization());
+            newEducation.setFromMonth(education.getFromMonth());
+            newEducation.setFromYear(education.getFromYear());
+            newEducation.setToMonth(education.getToMonth());
+            newEducation.setToYear(education.getToYear());
+            newEducation.setContinues(education.getContinues());
+
+            newEducation = educationRepository.save(newEducation);
+
+            return newEducation;
+        } else {
+            education = educationRepository.save(education);
+            return education;
+        }
+    }
+
+    public Education createEducation(Education education, User user) {
+        education.setUser(user);
+        education = educationRepository.save(education);
+        return education;
+    }
+
+    public void deleteEducation(Integer id) {
+        Optional<Education> educationOpt = educationRepository.findById(id);
+        if (educationOpt.isPresent()) {
+            educationRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Id education nie znalezione");
+        }
+    }
 }
