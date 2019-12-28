e518f107d6d8ba9eb1e69b13f2cf8789c4ba886d
pioste <pioste97@gmail.com>
2019-12-29 00:58:49 +0100

    commit 14

--- src/main/java/com/biuropracy/demo/controller/HomeController.java
+++ src/main/java/com/biuropracy/demo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.biuropracy.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,9 +9,14 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/")
+    @GetMapping(path = "/")
     public String hello(HttpServletRequest request) {
         System.out.println(request.getServletPath());
         return "index";
+    }
+
+    @GetMapping(path = "/all/calculator")
+    public String calculator() {
+        return "/all/calculator/calculatorForm";
     }
 }
