5b62cd26c3622c6e4de0998ab02ab9acb5c2ce4c
pioste <pioste97@gmail.com>
2020-01-20 22:56:03 +0100

    commit 38

--- src/main/java/com/biuropracy/demo/controller/HomeController.java
+++ src/main/java/com/biuropracy/demo/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.biuropracy.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,28 +12,31 @@ import javax.servlet.http.HttpServletRequest;
 public class HomeController {
 
     @GetMapping(path = "/")
-    public String hello(HttpServletRequest request) {
-        System.out.println(request.getServletPath());
+    public String hello() {
         return "index";
     }
 
-    @GetMapping(path = "/all/calculator")
-    public String calculator() {
+    @GetMapping(path = "/all/calculator/{salary}")
+    public String calculator(Model model, @PathVariable("salary") Integer salary) {
+        model.addAttribute("salary", salary);
         return "/all/calculator/calculatorForm";
     }
 
-    @GetMapping(path = "/user/calculator")
-    public String userCalculator() {
+    @GetMapping(path = "/user/calculator/{salary}")
+    public String userCalculator(Model model, @PathVariable("salary") Integer salary) {
+        model.addAttribute("salary", salary);
         return "/all/calculator/UserCalculator";
     }
 
-    @GetMapping(path = "/admin/calculator")
-    public String adminCalculator() {
+    @GetMapping(path = "/admin/calculator/{salary}")
+    public String adminCalculator(Model model, @PathVariable("salary") Integer salary) {
+        model.addAttribute("salary", salary);
         return "/all/calculator/adminCalculator";
     }
 
-    @GetMapping(path = "/employer/calculator")
-    public String employerCalculator() {
+    @GetMapping(path = "/employer/calculator/{salary}")
+    public String employerCalculator(Model model, @PathVariable("salary") Integer salary) {
+        model.addAttribute("salary", salary);
         return "/all/calculator/employerCalculator";
     }
 }
